75dcd2526700426c3b15092b4e4e3dc1d807e72f
Surendhar <mathisurendharl@gmail.com>
2026-08-12 23:13:08 +0530

    mail chack

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CommonModule } from '../common/common.module';
+
 import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
 
 @Module({
-  imports: [CommonModule],
   controllers: [ContactController],
   providers: [ContactService],
 })
