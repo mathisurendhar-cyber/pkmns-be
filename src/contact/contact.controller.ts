268d8ee2e58e03fdbe256f5113e43b1ca8ea2192
Surendhar <mathisurendharl@gmail.com>
2026-08-12 23:19:34 +0530

    mail chacking

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -5,38 +5,24 @@ import {
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
-
 import { ContactService } from './contact.service';
 
 @Controller('api')
 export class ContactController {
-  constructor(
-    private readonly contactService: ContactService,
-  ) {}
+  constructor(private readonly contactService: ContactService) {}
 
   @Post('contact-with-file')
   @UseInterceptors(
     FilesInterceptor('attachments', 10, {
       storage: memoryStorage(),
-
-      limits: {
-        fileSize: 25 * 1024 * 1024,
-      },
+      limits: { fileSize: 25 * 1024 * 1024 },
     }),
   )
   contact(
-    @Body()
-    body: {
-      name?: string;
-      mobile?: string;
-      message?: string;
-    },
-
-    @UploadedFiles()
-    files?: Express.Multer.File[],
+    @Body() body: { name?: string; mobile?: string; message?: string },
+    @UploadedFiles() files?: Express.Multer.File[],
   ) {
     return this.contactService.contact(body, files);
   }
